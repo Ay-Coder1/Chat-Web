d68c67167501150c70dad6f2a549e90dbda7a4f2
Ay-coder <ayomidebabarinde07@gmail.com>
2023-08-13 22:48:57 +0100

    update

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
 /* eslint-disable no-unused-vars */
-const dotenv = require('dotenv').config();
-const colors = require('colors');
+const dotenv = require("dotenv").config();
+const colors = require("colors");
 /* eslint-enable no-unused-vars */
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const credentialsMiddleware = require('./middleware/credentialsMiddleware');
-const { createSocketServer } = require('./config/socket');
-const connectToDataBase = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
-const messageRoutes = require('./routes/messageRoutes');
-
-const secretKey = process.env.SECRET;
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const credentialsMiddleware = require("./middleware/credentialsMiddleware");
+const { createSocketServer } = require("./config/socket");
+const connectToDataBase = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
 const PORT = process.env.PORT || 5000;
 
@@ -20,7 +18,7 @@ const app = express();
 app.use(credentialsMiddleware);
 app.use(cors(corsOptions));
 
-
+// check if any data is send to server if true, attach this data to request.body property
 app.use(express.json());
 
 // initialize socket.io server
@@ -29,25 +27,24 @@ const io = createSocketServer(server);
 global.io = io;
 
 app.use((req, res, next) => {
-   console.log(req.path, req.method);
-   next();
+  console.log(req.path, req.method);
+  next();
 });
 
-io.on('connection', socket => {
-   console.log(`Client connected with id: ${socket.id}`);
+io.on("connection", (socket) => {
+  console.log(`Client connected with id: ${socket.id}`);
 
-   socket.on('disconnect', () => {
-      console.log(`User: ${socket.id} disconnected`);
-   });
+  socket.on("disconnect", () => {
+    console.log(`User: ${socket.id} disconnected`);
+  });
 });
 
 // connect to Database
 connectToDataBase();
 
 // routes
-app.use('/api/users', userRoutes);
-app.use('/api/messages', messageRoutes);
-
+app.use("/api/users", userRoutes);
+app.use("/api/messages", messageRoutes);
 
 // Run production build via server-side
 // serve static front-end
@@ -65,7 +62,7 @@ app.use('/api/messages', messageRoutes);
 //    );
 // }
 
-// // start server
-// server.listen(PORT, () => {
-//    console.log(`Server listening at port: ${PORT}`.blue);
-// });
+// start server
+server.listen(PORT, () => {
+  console.log(`Server listening at port: ${PORT}`.blue);
+});
