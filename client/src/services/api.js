fd68bb82091d39013f71138d1ade88ed0a8b53b7
Ay-coder <ayomidebabarinde07@gmail.com>
2023-08-13 23:12:08 +0100

    update1

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -10,7 +10,7 @@ export const API_URL = {
   LOGIN_USER: `https://chat-web-3kav.onrender.com/api/users/login`,
   REGISTER_USER: `https://chat-web-3kav.onrender.com/api/users/signup`,
   GET_MESSAGES: `https://chat-web-3kav.onrender.com/messages`,
-  CREATE_MESSAGE: `https://chat-web-3kav.onrender.com/messages/`,
-  EDIT_MESSAGE: `https://chat-web-3kav.onrender.com/api/messages/`,
-  DELETE_MESSAGE: `https://chat-web-3kav.onrender.com/api/messages/`,
+  CREATE_MESSAGE: `https://chat-web-3kav.onrender.com/messages`,
+  EDIT_MESSAGE: `https://chat-web-3kav.onrender.com/api/messages`,
+  DELETE_MESSAGE: `https://chat-web-3kav.onrender.com/api/messages`,
 };
